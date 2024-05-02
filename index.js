4f90be6fc04b188ceef083d8841db61b9b93087f
adityaraval <adi7017@gmail.com>
2024-05-02 12:07:40 +0530

    dummy eslint package

--- index.js
+++
@@ -1,6 +0,0 @@
-const { concat } = require('lodash');
-exports.printMsg = function () {
-  const final = concat(1, 2, 3)
-  console.log(`This is a message from the demo package`);
-  console.log(final, "concat array values");
-}
---
+++ src/index.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: [
+    'airbnb-base',
+  ],
+  env: {
+    node: true,
+    es2021: true,
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'commonjs',
+  },
+};
